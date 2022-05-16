382579779a1c832ff4645c2452f5908f74077c94
vishalsingh384 <vishalsing384@gmail.com>
2022-05-16 19:26:02 +0530

    :construction: todo app

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var uid = new ShortUniqueId();
 const addBtn=document.querySelector(".add-btn");
 const modalCont=document.querySelector(".modal-cont");
 const allPriorityColors=document.querySelectorAll(".priority-color");
@@ -5,9 +6,13 @@ let colors=['lightpink','lightgreen','lightblue','black'];
 let modalPriorityColor=colors[colors.length - 1];//black
 // console.log(modalPriorityColor);
 let textAreaCont=document.querySelector(".textarea-cont");
-
 const mainCont=document.querySelector(".main-cont");
+let ticketsArr=[];
+let toolBoxColors = document.querySelectorAll(".color");//navbar colors
 
+
+
+//to open a closed modal container
 let isModalPresent=false;
 addBtn.addEventListener('click',function(){
     if(!isModalPresent){
@@ -20,6 +25,7 @@ addBtn.addEventListener('click',function(){
     isModalPresent=!isModalPresent;//toggling effect
 });
 
+//to remove and add active class from each priority color of modal container
 allPriorityColors.forEach(function(colorElement) {
     colorElement.addEventListener("click",function(){
         allPriorityColors.forEach(function(priorityColorElem){
@@ -31,6 +37,7 @@ allPriorityColors.forEach(function(colorElement) {
     });
 });
 
+//to generate and display a ticket
 modalCont.addEventListener("keydown",function(e){
     let key = e.key;
     if(key == "Shift"){
@@ -46,16 +53,80 @@ modalCont.addEventListener("keydown",function(e){
     }
 });
 
-function createTicket(modalPriorityColor,textAreaValue){
+//function to create a new ticket
+function createTicket(ticketColor,data,ticketId){
+    let id=ticketId || uid();
     let ticketCont=document.createElement("div");
     ticketCont.setAttribute("class","ticket-cont");
     ticketCont.innerHTML=`        
-    <div class="ticket-color ${modalPriorityColor}"></div>
+    <div class="ticket-color ${ticketColor}"></div>
     <div class="ticket-id"></div>
-    <div class="task-area">${textAreaValue}</div>
+    <div class="task-area">${data}</div>
     `;
 
     mainCont.appendChild(ticketCont);
+
+    //if ticket is being created for the first time , then ticketId would be undefined
+    if(!ticketId){
+        console.log("inside");
+        ticketsArr.push(
+            {
+                ticketColor,
+                data,
+                ticketId:id
+            }
+        );
+        localStorage.setItem("tickets",JSON.stringify(ticketsArr));
+    }
+};
+
+//get alltickets from local Storage
+if(localStorage.getItem("tickets")){
+    ticketsArr=JSON.parse(localStorage.getItem("tickets"));
+    ticketsArr.forEach(function(ticketObj){
+        createTicket(ticketObj.ticketColor,ticketObj.data,ticketObj.ticketId);
+    })
 }
+
+
+//filter tickets on the basis of ticketColor
+for(let i=0;i<toolBoxColors.length;i++){
+    toolBoxColors[i].addEventListener("click",function(e){
+        let currToolBoxColor=toolBoxColors[i].classList[0];
+        let filteredTickets=ticketsArr.filter(function(ticketObj){
+            return ticketObj.ticketColor==currToolBoxColor;
+        });
+
+        //remove all the tickets
+        let allTickets=document.querySelectorAll(".ticket-cont");
+        for(let i=0;i<allTickets.length;i++){
+            allTickets[i].remove();
+        }
+
+        filteredTickets.forEach(function(ticketObj){
+            createTicket(ticketObj.ticketColor,ticketObj.data,ticketObj.ticketId);
+        });
+    });
+
+    //to display all the tickets of all colors on double clicking
+    toolBoxColors[i].addEventListener("dblclick",function(){
+        //remove all the color specific tickets
+        let allTickets=document.querySelectorAll(".ticket-cont");
+        for(let i=0;i<allTickets.length;i++){
+            allTickets[i].remove();
+        }
+
+        //display all Tickets
+        ticketsArr.forEach(function(ticketObj){
+            createTicket(ticketObj.ticketColor,ticketObj.data,ticketObj.ticketId);
+        });
+    });
+}
+
+
+
+
+
+
 
 
